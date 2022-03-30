d57e6fbdd2a874fe2fc6cfcbf4d8036ab822d4e2
Artem Goncharov <Artem.goncharov@gmail.com>
2022-03-30 11:26:37 +0200

    fix names (#1)
    fix names
    
    Reviewed-by: kucerakk <kucerakk@gmail.com>

--- umn/source/conf.py
+++ umn/source/conf.py
@@ -22,8 +22,8 @@ html_theme_options = {
 }
 otcdocs_auto_name = False
 otcdocs_auto_version = False
-project = 'Dummy Service'  # FIXME
-otcdocs_repo_name = 'opentelekomcloud-docs/template'  # FIXME
+project = 'Web Application Firewall Service'
+otcdocs_repo_name = 'opentelekomcloud-docs/web-application-firewall'
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -147,11 +147,11 @@ html_title = "Dummy UMN"  # FIXME
 # html_file_suffix = ''
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'dummydoc'  # FIXME
+htmlhelp_basename = 'wafdoc'
 
 latex_documents = [
     ('index',
-     'umn-dummy.tex',  # FIXME
+     'umn-waf.tex',
      u'%s User Manual Documentation' % project,
      u'OpenTelekomCloud', 'manual'),
 ]
